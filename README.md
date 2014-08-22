3b292d2cd27383c61c5a654f767ea7c7a2bc2ed5
unknown <rudi@bvgrudi1.bosveld.co.za>
2014-08-22 10:58:13 +0200

    Finished

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ This repository contains :
 
 The data used for the course project can be found [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
 
-The run_analysis.R script should be run in the same directory as the original dataset, that is, on the same level as README.txt
+The run_analysis.R script should be run in the same directory as the original dataset, that is, on the same level as README.txt, next to the "train" directory
 
 The run_analysis.R script generates two new data sets :
 * tidy_data1.txt - A merge of the training and test sets containing only the features related to the mean and standard deviation measurements in the original data set(inertial signal data not included). This data set also contains the activity label and subject for each observation
-* tidy_data2.txt - Another merge of the training and test sets containing the average of all features in the original data set, grouped by activity\subject. This data set includes the inertial signal data
+
+* tidy_data2.txt - Another merge of the training and test sets containing the average of all features in the original data set, grouped by activity and subject(in wide format)
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 #######################################################################
-#Step 1 - Merge the training and the test sets to create one data set#
+#Read the training and test data and merge  to create one data set
 #######################################################################
 
 #Load training data from the 'train' directory
@@ -9,35 +9,56 @@ X_train <- read.table("./train/X_train.txt")
 X_test <- read.table("./test/X_test.txt")
 
 #Merge the training and test sets
+#note : train before test
 X_all <- rbind(X_train, X_test)
 
 
 #######################################################################
-#Step 2 - Extract only the measurements on the mean and standard     #
-#deviation for each measurement                                       #
+#Read the feature names. Clean feature names, and extract indices of
+#mean and standard deviation features
 #######################################################################
 
 #Load the feature names from features.txt
 featureNames <- as.character(read.table("features.txt")[,2])
+
 #For some reason a few of the feature names in this file contain 'BodyBody',
 #correct that by replacing by 'Body'
 featureNames <- gsub(pattern="BodyBody", replacement="Body", x=featureNames, fixed=TRUE)
 
-#Filter the feature names to only include those that are mean or standard deviation measurements
-#First find the indices of the feature names containing...
-#"-mean()"
-mean_feature_indices <- grep("-mean()", featureNames, fixed=TRUE)
-#or containing "-std()"
-std_feature_indices <- grep("-std()", featureNames, fixed=TRUE)
-#generate the new names for mean() type features by removing "mean()" and preceding with "m"
-mean_feature_names <- paste("m", gsub("-mean()", "", featureNames[mean_feature_indices], fixed=TRUE), sep="")
-#generate the new names for std() type features by removing "std()" and preceding with "s"
-std_feature_names <- paste("s", gsub("-std()", "", featureNames[std_feature_indices], fixed=TRUE), sep="")
+#Characters like '-', ',' , '(' and ')' in the feature names may cause 
+#downstream problems, so replace them
+#replace '-' by '_'
+featureNames <- gsub(pattern="-", replacement="_", x=featureNames, fixed=TRUE)
+#replace ',' by '_'
+featureNames <- gsub(pattern=",", replacement="_", x=featureNames, fixed=TRUE)
+#replace '()' by '_'
+featureNames <- gsub(pattern="()", replacement="_", x=featureNames, fixed=TRUE)
+#replace '(' by '_'
+featureNames <- gsub(pattern="(", replacement="_", x=featureNames, fixed=TRUE)
+#replace ')' by '_'
+featureNames <- gsub(pattern=")", replacement="_", x=featureNames, fixed=TRUE)
+#these replacements cause '__' in some feature names, replace by a single '_'
+featureNames <- gsub(pattern="__", replacement="_", x=featureNames, fixed=TRUE)
+
+#Find the indices of the mean and standard deviation feature names by looking for
+#"_mean_"
+mean_feature_indices <- grep("_mean_", featureNames, fixed=TRUE)
+#and "_std_"
+std_feature_indices <- grep("_std_", featureNames, fixed=TRUE)
+#generate  new names for mean type features by replacing '_mean_' with '_' and preceding with 'm'
+mean_feature_names <- paste("m", gsub("_mean_", "_", featureNames[mean_feature_indices], fixed=TRUE), sep="")
+#generate the new names for std type features by replacing '_std_' with '_' and preceding with 's'
+std_feature_names <- paste("s", gsub("_std_", "_", featureNames[std_feature_indices], fixed=TRUE), sep="")
+
+#finally, we end up with some feature names ending in '_', fix that
+mean_feature_names <- sub(pattern="_$", replacement="",x=mean_feature_names)
+std_feature_names <- sub(pattern="_$", replacement="",x=std_feature_names)
+featureNames <- sub(pattern="_$", replacement="",x=featureNames)
 
 
 #######################################################################
-#Step 3 - Use descriptive activity names to name the activities in    #
-#the data set                                                         #
+#Read the activity labels for both train and test. Create a factor
+#variable using the textual descriptions for the activities
 #######################################################################
 
 #Read descriptions of the activities from activity_labels.txt as a character vector
@@ -51,10 +72,13 @@ label_test <- read.table("./test/y_test.txt")[,1]
 
 #merge the train and test labels as a factor variable
 #use activityLabelDescriptions as labels for the factor
+#note : train before test
 label_all <- factor(c(label_train, label_test), labels=activityLabelDescriptions)
 
 #######################################################################
-#Create the first data set, tidy_data1.txt                            #
+#Create and save the first tidy data set, tidy_data1.txt
+#This data set contains features for mean and standard deviation 
+#measurements, as well as subject and activity
 #######################################################################
 
 #load the train subjects
@@ -64,21 +88,38 @@ subject_train <- read.table("./train/subject_train.txt")[,1]
 subject_test <- read.table("./test/subject_test.txt")[,1]
 
 #merge the train and test subjects
+#note : train before test
 subject_all <- c(subject_train, subject_test)
 
-#now build the first data set
+#now start building the first data set
 #use the mean() and std() feature indices to subset
+#note : mean features before std features
 tidy_data1 <- X_all[,c(mean_feature_indices, std_feature_indices)]
 
-#name the features
+#name the features in the dataset
+#note : mean features before std features
 names(tidy_data1) <- c(mean_feature_names, std_feature_names)
 
-#add the subjects
+#add the subjects as a feature named 'subject'
 tidy_data1 <- cbind(tidy_data1, subject=subject_all)
 
-#add the activity labels
+#add the activity labels factor as a feature named 'activity'
 tidy_data1 <- cbind(tidy_data1, activity=label_all)
 
-#write the data set to file
-write.table(file="tidy_data.txt", x=tidy_data1, row.names=FALSE)
+#and finally, write the data set to file
+write.table(file="tidy_data1.txt", x=tidy_data1, row.names=FALSE)
 
+#######################################################################
+#Create and save the second data set, tidy_data2.txt
+#This data set contains averages for each feature, grouped by each
+#activity and each subject
+#######################################################################
+
+#aggregate over all features in tidy_data1 except subject and activity, using the mean function,
+#grouping by subject and activity. Use different names for the group by features to avoid confusion
+tidy_data2 <- aggregate(x=tidy_data1[,1:66], 
+                        by=list("forSubject"=tidy_data1$subject, "forActivity"=tidy_data1$activity), 
+                        FUN=mean)
+
+#write the data set to file
+write.table(file="tidy_data2.txt", x=tidy_data2, row.names=FALSE)
