cd62ba6ec1d40d6b687f14c3112d878b4d9fc514
unknown <washier@gmail.com>
2014-08-22 07:30:39 +0200

    more work

--- run_analysis.R
+++ run_analysis.R
@@ -2,13 +2,13 @@
 #Step 1 - Merge the training and the test sets to create one data set#
 #######################################################################
 
-#Load training data from /train/X_train.txt
+#Load training data from the 'train' directory
 X_train <- read.table("./train/X_train.txt")
 
-#Load test data from /test/X_train.txt
+#Load test data from the 'test' directory
 X_test <- read.table("./test/X_test.txt")
 
-#Merge the data sets
+#Merge the training and test sets
 X_all <- rbind(X_train, X_test)
 
 
@@ -19,9 +19,12 @@ X_all <- rbind(X_train, X_test)
 
 #Load the feature names from features.txt
 featureNames <- as.character(read.table("features.txt")[,2])
+#For some reason a few of the feature names in this file contain 'BodyBody',
+#correct that by replacing by 'Body'
+featureNames <- gsub(pattern="BodyBody", replacement="Body", x=featureNames, fixed=TRUE)
 
 #Filter the feature names to only include those that are mean or standard deviation measurements
-#Do this by finding the indices of the feature names containing
+#First find the indices of the feature names containing...
 #"-mean()"
 mean_feature_indices <- grep("-mean()", featureNames, fixed=TRUE)
 #or containing "-std()"
@@ -40,15 +43,15 @@ std_feature_names <- paste("s", gsub("-std()", "", featureNames[std_feature_indi
 #Read descriptions of the activities from activity_labels.txt as a character vector
 activityLabelDescriptions <- as.character(read.table("activity_labels.txt")[,2])
 
-#Load the activity labels for train
-y_train <- read.table("./train/y_train.txt")[,1]
+#Load the activity labels from the 'train' directory
+label_train <- read.table("./train/y_train.txt")[,1]
 
-#Load the activity labels for test
-y_test <- read.table("./test/y_test.txt")[,1]
+#Load the activity from the 'test' directory
+label_test <- read.table("./test/y_test.txt")[,1]
 
 #merge the train and test labels as a factor variable
 #use activityLabelDescriptions as labels for the factor
-y_all <- factor(c(y_train, y_test), labels=activityLabelDescriptions)
+label_all <- factor(c(label_train, label_test), labels=activityLabelDescriptions)
 
 #######################################################################
 #Create the first data set, tidy_data1.txt                            #
@@ -74,7 +77,7 @@ names(tidy_data1) <- c(mean_feature_names, std_feature_names)
 tidy_data1 <- cbind(tidy_data1, subject=subject_all)
 
 #add the activity labels
-tidy_data1 <- cbind(tidy_data1, activity=y_all)
+tidy_data1 <- cbind(tidy_data1, activity=label_all)
 
 #write the data set to file
 write.table(file="tidy_data.txt", x=tidy_data1, row.names=FALSE)
